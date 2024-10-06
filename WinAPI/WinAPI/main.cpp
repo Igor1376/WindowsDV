b0f9de37b0fe6ab6fee54cec8c961e5d91c6b990
Belya_uu43@top-academy.ru <Belya_uu43@top-academy.ru>
2024-10-06 22:05:30 +0300

    complete

--- WinAPI/WinAPI/main.cpp
+++ WinAPI/WinAPI/main.cpp
@@ -1,11 +1,44 @@
 #include<Windows.h>
+#include"resource.h"
+
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParm, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-	
-	MessageBox(NULL, 
-		"Hello Windows!\nThis is MessageBox()", 
-		"Window title", 
-		MB_CANCELTRYCONTINUE | MB_HELP | MB_DEFBUTTON3);
+
+	/*MessageBox(NULL,
+		"Hello Windows!\nThis is MessageBox()",
+		"Window title",
+		MB_CANCELTRYCONTINUE | MB_HELP | MB_DEFBUTTON3);*/
+
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, DlgProc, 0);
 	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDC_BUTTON_COPY:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+
+
+		}
+		case IDOK: MessageBox(hwnd, "Была нажата кнопка ok", "Info", MB_OK | MB_ICONINFORMATION); break;
+		case IDCANCEL: EndDialog(hwnd, 0); break;
+		}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+		break;
+	}
+	return FALSE;
 }
